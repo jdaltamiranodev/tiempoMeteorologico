edfe326318bcd3226b9342a2fba2e1b0618f548f
jhonsdb <davilajhon08@gmail.com>
2024-04-09 11:55:04 +0200

    instalacion de tailwind

--- vue-tiempo-climatico/src/App.vue
+++ vue-tiempo-climatico/src/App.vue
@@ -2,13 +2,17 @@
   <div class="flex flex-col min-h-screen font-roboto bg-primary-light">
     <SiteNavigation/>
     <RouterView/>
+    
   </div>
+ <Footer />
+ 
 </template>
 
 <script setup>
 
 import { RouterView } from "vue-router";
 import SiteNavigation from "./components/SiteNavigation.vue";
+import Footer from "./components/PartFooter.vue"
 
 </script>
 
--- vue-tiempo-climatico/tailwind.config.js
+++ vue-tiempo-climatico/tailwind.config.js
@@ -1,24 +1,14 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html",".src/**/*.{vue,js,ts,jsx,tsx}"],
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+    "./node_modules/flowbite/**/*.js",
+  ],
   theme: {
-    extend: {
-      colors:{
-        "primary-light":"#00668A",
-        "secundary-dark":"#004E71",
-      },
-    },
-    fontFamily:{roboto:["Roboto, sans-serif"],
-   },  
-   container:{
-    padding:"2rem",
-    center: true,
-   },
-   screens: {
-    sm:"640px",
-    md:"768px",
-   },
+    extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('flowbite/plugin')
+  ],
 }
-
