2fe385e000bdf84ded23c957d9017f4b540a17f3
David Altamirano <jdaltamiranodev@gmail.com>
2024-04-16 11:31:43 +0200

    se elimina parte innecesaria del div contenedor

--- vue-tiempo-climatico/src/App.vue
+++ vue-tiempo-climatico/src/App.vue
@@ -1,9 +1,5 @@
 <template>
-<<<<<<< HEAD
-  <div class="flex flex-col min-h-screen font-roboto black">
-=======
   <div class="flex flex-col min-h-screen font-roboto bg to-weather-primary">
->>>>>>> 3c4b315563cf939846efb81f34f23f98b30a706f
     <SiteNavigation/>
     <HomeView/>
     <RouterView/>
