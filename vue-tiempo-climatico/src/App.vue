f35993eac7b3d757b4a0f52a10a264ef44b7c4fd
David Altamirano <jdaltamiranodev@gmail.com>
2024-04-17 11:02:18 +0200

    Se deja listo dev para incorporar desarrollos

--- vue-tiempo-climatico/src/App.vue
+++ vue-tiempo-climatico/src/App.vue
@@ -1,17 +1,9 @@
 <template>
-  <div class="flex flex-col min-h-screen font-roboto bg to-weather-primary">
-    <SiteNavigation/>
-    <HomeView/>
-    <RouterView/>
-    
+  <div >
+      
   </div>
- <Footer />
- 
 </template>
 
 <script setup>
-
-import { RouterView } from "vue-router";
-import SiteNavigation from "./components/SiteNavigation.vue";
 
 </script>
